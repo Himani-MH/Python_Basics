a35f808f7346eedcad5697106935c35de9b5e4d9
Himani <57355796+Himani-MH@users.noreply.github.com>
2020-10-05 19:16:32 +0530

    Readme for_loop.py Updates
    Made the program more efficient.
    User can enter the number and the number of multiples according to his needs.

--- python_programs/for_loop.py
+++ python_programs/for_loop.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-number = 9
-for i in range(1, 5):
+print("Enter the number whose multiples you want to find =")
+number = int(input())
+print("Enter the number of multiples you want to find =")
+multiples = int(input())
+for i in range(1, (multiples+1)):
     mul_table = number * i
     print("{} * {} = {}".format(number, i, mul_table))
